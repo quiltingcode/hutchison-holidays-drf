c32fc468e07a5cf0aa67eaa30fcc59ddc08b4407
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2024-01-20 11:35:04 +0000

    Install cloudinary and connect url

--- hutch_hols/settings.py
+++ hutch_hols/settings.py
@@ -16,6 +16,11 @@ import os
 if os.path.exists('env.py'):
     import env
 
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +46,9 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    'cloudinary_storage',
     "django.contrib.staticfiles",
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
