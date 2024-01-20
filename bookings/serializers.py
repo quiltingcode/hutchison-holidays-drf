655e34813be0fcdf7d4665c9d8d816829d7319ad
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2024-01-20 14:42:37 +0000

    Add natural time to convert dates

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Booking
-
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 class BookingSerializer(serializers.ModelSerializer):
     """
@@ -12,12 +12,19 @@ class BookingSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(
         source='owner.profile.profile_pic.url'
     )
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
 
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
     class Meta:
         model = Booking
         fields = [
