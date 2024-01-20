848f35914289ac4bb11a5419aeae765ac341dd72
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2024-01-20 14:30:36 +0000

    Updatesettings for simple jwt

--- hutch_hols/settings.py
+++ hutch_hols/settings.py
@@ -64,6 +64,20 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [( 
+        'rest_framework.authentication.SessionAuthentication' 
+        if 'DEV' in os.environ 
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+    }
+
+REST_USE_JWT = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_SECURE = True
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+USE_JWT = True
+
 MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",
     "django.middleware.security.SecurityMiddleware",
